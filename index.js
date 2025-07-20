8aa22a00e7dc80b398d24af92e73456d68235f37
Marcelo Pires de Oliveira <119169432+marcelowkr2@users.noreply.github.com>
2025-07-19 22:24:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-j\hjdhsajhdsajhjdhasdasfas
-sfsdfsfsfs
-dasfadfaf
-rgdfgdf
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import  store  from './app/store';
+
+const cotainer = document.getElementById("root");
+const root = createRoot(cotainer);
+
+root.render(<Provider store={store}><App /></Provider>);
